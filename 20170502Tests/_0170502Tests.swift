abdf5a3f6f15ebe3d9dec6ac079d7ac7ab510f69
“jasmine888tw” <jasmine888tw@gmail.com>
2017-05-02 15:44:01 +0800

    Initial Commit

---
+++ 20170502Tests/_0170502Tests.swift
@@ -0,0 +1,36 @@
+//
+//  _0170502Tests.swift
+//  20170502Tests
+//
+//  Created by juicemeart on 2017/5/2.
+//  Copyright © 2017年 juicemeart. All rights reserved.
+//
+
+import XCTest
+@testable import _0170502
+
+class _0170502Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
