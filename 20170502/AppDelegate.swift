076904730cb86345c0a442de666d14b349403de3
“jasmine888tw” <jasmine888tw@gmail.com>
2017-05-03 10:38:50 +0800

    1st

--- 20170502/AppDelegate.swift
+++ 20170502/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UITabBar.appearance().tintColor = UIColor(red: 0, green: 210/255, blue: 44/255, alpha: 1)
         return true
     }
 
